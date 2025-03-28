8a6b4301ce31ee0734784e9a36fd5ffce5522314
A.karthick <karthicka293@gmail.com>
2025-03-28 11:59:37 +0530

    Update StackLinklist.java

--- StackLinklist.java
+++ StackLinklist.java
@@ -1,181 +1,43 @@
 import java.util.*;
-
-class Node
-
-{
-
-int data;
-
-Node next;
-
-Node(int data)
-
-{
-
-this.data=data;                                  
-
-this.next=null;
-
-}
-
-}
-
-class StackLinkedlist
-
-{
-
-private Node top;
-
-public StackLinkedlist ()
-
-{
-
-this.top=null;
-
-}
-
-public void push(int data)
-
-{
-
-Node newNode=new Node(data);
-
-newNode.next=top;
-
-top=newNode;
-
-}
-
-public void pop()
-
-{
-
-if(isEmpty())
-
-{
-
-System.out.println("Stack is empty");
-
-}
-
-else
-
-{
-
-int poppedData=top.data;
-
-top=top.next;
-
-System.out.println("Popped element is:"+poppedData);
-
-}
-
-}
-
-public boolean isEmpty()
-
-{
-
-return top==null;
-
-}
-
-public void display()
-
-{
-
-if(isEmpty())
-
-{
-
-System.out.println("stack is empty");                  
-
-return;
-
-}
-
-Node current=top;
-
-while(current!=null)
-
-{
-
-System.out.print(current.data+ " ");
-
-current=current.next;
-
-}
-
-System.out.println();
-
-}
-
-public static void main(String[] args)
-
-{
-
-Scanner sc=new Scanner(System.in);
-
-StackLinkedlist stack=new StackLinkedlist();
-
-System.out.println("1.PUSH");
-
-System.out.println("2.POP");
-
-System.out.println("3.DISPLAY");
-
-System.out.println("4.EXIT");
-
-while(true)
-
-{
-
-System.out.println("Enter your choice:");
-
-int choice=sc.nextInt();
-
-switch(choice)
-
-{
-
-case 1:
-
-System.out.println("Enter value to push into stack");
-
-int value=sc.nextInt();
-
-stack.push(value);
-
-break;
-
-case 2:
-
-stack.pop();
-
-break;
-
-case 3:
-
-stack.display();
-
-break;
-
-case 4:
-
-System.out.println("exiting");
-
-sc.close();
-
-return;
-
-default:
-
-System.out.println("inavlid choice please try again");
-
-}
-
-}
-
-}
-
+class StackLinkedlist {
+    private static class Node {
+        int data;
+        Node next;
+        Node(int data) { this.data = data; }
+    }
+    private Node top;
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+    }
+    public void pop() {
+        if (top == null) System.out.println("Stack is empty");
+        else {
+            System.out.println("Popped element: " + top.data);
+            top = top.next;
+        }
+    }
+    public void display() {
+        if (top == null) System.out.println("Stack is empty");
+        else {
+            for (Node curr = top; curr != null; curr = curr.next)
+                System.out.print(curr.data + " ");
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StackLinkedlist stack = new StackLinkedlist();
+        while (true) {
+            System.out.println("1.PUSH 2.POP 3.DISPLAY 4.EXIT\nEnter choice:");
+            switch (sc.nextInt()) {
+                case 1 -> { System.out.println("Enter value:"); stack.push(sc.nextInt()); }
+                case 2 -> stack.pop();
+                case 3 -> stack.display();
+                case 4 -> { sc.close(); return; }
+                default -> System.out.println("Invalid choice, try again");
+            }
+        }
+    }
 }
