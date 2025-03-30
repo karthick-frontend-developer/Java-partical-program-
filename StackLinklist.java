3d3322a6a0877b7b27ee602f5d6b4473ac42198d
A.karthick <karthicka293@gmail.com>
2025-03-30 18:45:20 +0530

    Update StackLinklist.java

--- StackLinklist.java
+++ StackLinklist.java
@@ -1,10 +1,10 @@
 import java.util.*;
-class StackLinkedlist {
-    private static class Node {
-        int data;
-        Node next;
-        Node(int data) { this.data = data; }
-    }
+class Node {
+    int data;
+    Node next;
+    Node(int data) { this.data = data; }
+}
+class StackLinkedList {
     private Node top;
     public void push(int data) {
         Node newNode = new Node(data);
@@ -19,20 +19,18 @@ class StackLinkedlist {
         }
     }
     public void display() {
-        if (top == null) System.out.println("Stack is empty");
-        else {
-            for (Node curr = top; curr != null; curr = curr.next)
-                System.out.print(curr.data + " ");
-            System.out.println();
-        }
+        for (Node cur = top; cur != null; cur = cur.next)
+            System.out.print(cur.data + " ");
+        System.out.println(top == null ? "Stack is empty" : "");
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        StackLinkedlist stack = new StackLinkedlist();
+        StackLinkedList stack = new StackLinkedList();  
         while (true) {
-            System.out.println("1.PUSH 2.POP 3.DISPLAY 4.EXIT\nEnter choice:");
+            System.out.println("1.PUSH 2.POP 3.DISPLAY 4.EXIT");
+            System.out.println("Enter your choice:");
             switch (sc.nextInt()) {
-                case 1 -> { System.out.println("Enter value:"); stack.push(sc.nextInt()); }
+                case 1 -> stack.push(sc.nextInt());
                 case 2 -> stack.pop();
                 case 3 -> stack.display();
                 case 4 -> { sc.close(); return; }
